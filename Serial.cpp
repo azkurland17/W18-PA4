294a6dca77663f23e3bc83bcd1081c66e85734f4
Alexandra Z Kurland <azkurlan@ieng6-202.ucsd.edu>
2018-03-08 12:39:08 -0800

    part 1

--- Serial.cpp
+++ Serial.cpp
@@ -1,7 +1,7 @@
 // CSE 101 Winter 2018, PA 4
 //
-// Name: TODO put both partners' info if applicable
-// PID: TODO
+// Name: Alexandra Kurland 
+// PID: A92137786
 // Sources of Help: TODO
 // Due: March 10th, 2018 at 11:59 PM
 
@@ -11,6 +11,33 @@
 #include "Serial.hpp"
 
 int serial(std::vector<int> nums) {
-  return 0;
+	int size = nums.size();
+	int firstVal = nums[0];
+    int splitInd = size/2;
+	int startInd = 0;
+	int endInd = size -1;	
+	
+	while(1){
+		if(size == 1){
+			return nums[splitInd];
+		}
+		else if(nums[splitInd] == firstVal+splitInd){
+			//check from split+1 to end 	
+			if(splitInd != nums.size()-1){
+				startInd = splitInd + 1;
+				size = endInd +1 - startInd;
+				splitInd = (size/2) + startInd;
+			}
+		}
+		else if(nums[splitInd] < firstVal+splitInd){
+			if(splitInd != 0){
+				endInd = splitInd -1;
+				size = endInd + 1 - startInd;
+				splitInd = (size/2) + startInd; 
+			}	
+		}
+	}
+	return 0;
 }
+
 #endif
