75e44e46b567ebd6f940ba4ba14a3e6cbde24cca
Alexandra Z Kurland <azkurlan@ieng6-202.ucsd.edu>
2018-03-09 00:02:16 -0800

    happy birthday

--- Buildings.cpp
+++ Buildings.cpp
@@ -10,54 +10,68 @@
 
 #include "Buildings.hpp"
 #include <queue>
-
+#include <iostream>
 //run a bfs type of algorithm to trace the buildings and replace 1s with 0s
 int buildings(TwoD_Array<int> survey) {
   int buildings = 0;
-  TwoD_Array<int> * ar = &survey;
   int row = survey.getNumRows();
   int col = survey.getNumCols();
   for(int rInd = 0; rInd < row; rInd++){
   	for(int cInd = 0; cInd < col; cInd++ ){
-  		if(survey.at(rInd,cInd) == 1){
+		if(survey.at(rInd,cInd) == 1){
   		//if one then delete and run bfs!!
+  		//survey.printOut();
   		survey.at(rInd,cInd) = 0;
   		std::queue<std::pair<int,int>> q; 
   		std::pair<int,int> p(rInd,cInd);
   		q.push(p);
   		std::pair<int,int> curr;
-  			while(!q.empty()){
+  			while(!q.empty()){	
   				curr = q.front();
-  				q.pop();
+  			    int cIndx = curr.second;
+				int rIndx = curr.first;
+		//		std::cout<<"curr = "<<rIndx<< " , "<<cIndx<<std::endl;
+				survey.at(rIndx,cIndx) = 0;
+				//survey.printOut();
+				//std::cout<<"poop: "<<curr.first<<std::endl;
+				q.pop();
   				//for all neighbors of curr
-  				if(cInd < size -1 ){
+  				if(cIndx < col -1 ){
   					//check if one over right is one
-  					if(survey.at(cInd+1, rInd) == 1){
-  						q.push(std::pair<int,int> d(cInd+1, rInd));
+  					if(survey.at(rIndx, cIndx+1) == 1){
+  						std::pair<int,int> d(rIndx, cIndx+1);
+						q.push(d);
+		//				std::cout<<"right"<<std::endl;
   					}	
   				}
-  				if(cInd > 0 ){
+  				if(cIndx > 0 ){
   					//check if one over left is one
-  					if(survey.at(cInd-1, rInd) == 1){
-  						q.push(std::pair<int,int> d(cInd-1, rInd));
+  					if(survey.at(rIndx, cIndx-1) == 1){
+  						std::pair<int,int> d(rIndx, cIndx-1);
+						q.push(d);
+		//				std::cout<<"left"<<std::endl;
   					}	
   				}
-  				if(rInd > 0 ){
+  				if(rIndx > 0 ){
   					//check if one above
-  					if(survey.at(cInd, rInd+1) == 1){
-  						q.push(std::pair<int,int> d(cInd, rInd+1));
-  					}	
+  					if(survey.at(rIndx-1, cIndx)  == 1){
+  						std::pair<int,int> d( rIndx-1, cIndx);
+  						q.push(d);
+		//				std::cout<<"up"<<std::endl;
+					}	
   				}
-  				if(rInd < row -1 ){
+  				if(rIndx < row -1 ){
   					//check if one below
-  					if(survey.at(cInd, rInd-1) == 1){
-  						q.push(std::pair<int,int> d(cInd, rInd-1));
+  					if(survey.at(rIndx+1, cIndx) == 1){
+  						std::pair<int,int> d(rIndx+1,cIndx);
+		//				std::cout<<"heredksfljdlsfjvfslsfjf"<<std::endl;
+						q.push(d);
   					}	
   				}
 
   			}
-  		}
-  		buildings = buildings + 1;
+  		buildings++;
+		}
   	}
    }
    return buildings;	
